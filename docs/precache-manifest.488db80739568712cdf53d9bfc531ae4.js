88746699f08f2588af3be6a49a908a762f1c5371
sanjisand <ssaurexd@gmail.com>
2020-06-25 16:35:34 -0500

    Se agrego docs

---
+++ docs/precache-manifest.488db80739568712cdf53d9bfc531ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26dac52dfc257b50c5165278ec04621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bbd1d433c7e6b130394",
+    "url": "/static/css/main.86ba9d2b.chunk.css"
+  },
+  {
+    "revision": "5b587aa55be52d26eb30",
+    "url": "/static/js/2.3dba92cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3dba92cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbd1d433c7e6b130394",
+    "url": "/static/js/main.7c99f400.chunk.js"
+  },
+  {
+    "revision": "93faf900113da627a69c",
+    "url": "/static/js/runtime-main.0f89b026.js"
+  }
+]);
